9f2128d159f18efa11d1b1e0701e417ecac3bcbe
Илья Тясин <turbopad7200@gmail.com>
2024-11-23 12:36:48 +0500

    Add project files.

---
+++ Tyuiu.TiasinIA.Sprint6.Task0.V3/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.TiasinIA.Sprint6.Task0.V3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
