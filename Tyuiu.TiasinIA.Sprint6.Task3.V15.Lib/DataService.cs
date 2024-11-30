951957e4f74b12f5b3ad47bab5a53026e9fbb7a9
Илья Тясин <turbopad7200@gmail.com>
2024-11-30 15:15:10 +0500

    Task3

--- Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib/DataService.cs
+++ Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib
             int[] tColumn = new int[rows];
             for (int i = 0; i < rows; i++)
             {
-                tColumn[i] = matrix[i, 1];
+                tColumn[i] = matrix[i, 0];
             }
 
 
@@ -21,11 +21,11 @@ namespace Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib
             int[,] sortedArray = new int[rows, cols];
             for (int i = 0; i < rows; i++)
             {
-                sortedArray[i, 1] = tColumn[i];
+                sortedArray[i, 0] = tColumn[i];
                 for (int j = 1; j < cols; j++)
                 {
 
-                    int originalRowIndex = Array.IndexOf(tColumn, sortedArray[i, 1]);
+                    int originalRowIndex = Array.IndexOf(tColumn, sortedArray[i, 0]);
                     sortedArray[i, j] = matrix[originalRowIndex, j];
                 }
             }
