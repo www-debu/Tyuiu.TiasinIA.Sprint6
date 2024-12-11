03a404770df85d77e42fc6e9153885c1d4d6f4ff
Илья Тясин <turbopad7200@gmail.com>
2024-12-11 16:15:14 +0500

    Task3

--- Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib/DataService.cs
+++ Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib/DataService.cs
@@ -5,34 +5,33 @@ namespace Tyuiu.TiasinIA.Sprint6.Task3.V15.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
 
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
 
-            int[,] tempArray = new int[rows, cols];
-
+            int[] firstColumn = new int[rows];
             for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < cols; j++)
-                {
-                    tempArray[i, j] = matrix[i, j];
-                }
+                firstColumn[i] = matrix[i, 0];
             }
 
 
-            Array.Sort(tempArray, (a, b) => a[2].CompareTo(b[2]));
+            Array.Sort(firstColumn);
 
 
+            int[,] sortedArray = new int[rows, cols];
             for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < cols; j++)
+                sortedArray[i, 0] = firstColumn[i];
+                for (int j = 1; j < cols; j++)
                 {
-                    matrix[i, j] = tempArray[i, j];
+
+                    int originalRowIndex = Array.IndexOf(firstColumn, sortedArray[i, 0]);
+                    sortedArray[i, j] = matrix[originalRowIndex, j];
                 }
             }
 
-            return matrix;
+            return sortedArray;
         }
     }
 }
