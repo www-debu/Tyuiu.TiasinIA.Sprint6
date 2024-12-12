794fb0a8cd9ee153d89a477e3a0df467642cf7fd
Илья Тясин <turbopad7200@gmail.com>
2024-12-12 18:29:17 +0500

    Task6

---
+++ Tyuiu.TiasinIA.Sprint6.Task6.V8.Lib/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.TiasinIA.Sprint6.Task6.V8.Lib
+{
+    public class Class1 : ISprint6Task6V8
+    {
+        public string CollectTextFromFile(string path)
+        {
+                StringBuilder result = new StringBuilder();
+
+                try
+                {
+                    string input;
+                    using (var reader = new StreamReader(path))
+                    {
+                        input = reader.ReadToEnd();
+                    }
+
+
+                    var words = input.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+                    var wordsWithLowercaseB = new List<string>();
+
+
+                    foreach (var word in words)
+                    {
+                        if (word.Contains('z'))
+                        {
+                            wordsWithLowercaseB.Add(word);
+                        }
+                    }
+                    return string.Join(" ", wordsWithLowercaseB);
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Ошибка: {ex.Message}", ex);
+                }
+        }
+    }
+}
---
+++ Tyuiu.TiasinIA.Sprint6.Task6.V8/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.TiasinIA.Sprint6.Task6.V8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
